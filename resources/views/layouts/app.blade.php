4e2cd5d033c81d2fcbb84348f7088d2720edee8f
Ichsan <ichsanfirdaus99@gmail.com>
2022-10-26 03:48:45 +0700

    update page title name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Todos</title>
+    <title>{{ env('APP_NAME') }}</title>
     <link rel="stylesheet" href="{{ asset('assets/css/main/app.css') }}">
     <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.svg') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('assets/css/shared/iconly.css') }}">
