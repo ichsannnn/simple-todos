d4c45526045af6c38f73be8251989bd2faf6c70f
Ichsan <ichsanfirdaus99@gmail.com>
2022-10-25 16:37:51 +0700

    initial commit

---
+++ app/Http/Livewire/Todo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Todo extends Component
+{
+    public function render()
+    {
+        return <<<'blade'
+            <div>
+                {{-- The best athlete wants his opponent at his best. --}}
+            </div>
+        blade;
+    }
+}
